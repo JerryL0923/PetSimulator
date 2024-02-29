b455b25692104d8e68f57b416d7b313c42317f71
JerryL0923 <jerrylamemail@gmail.com>
2024-02-29 13:48:20 -0800

    initialize PetSimulator

---
+++ PetSimulator.java
@@ -0,0 +1,39 @@
+/*
+ * Activity 3.6.3
+ */
+public class PetSimulator
+{
+  public static void main(String[] args)
+  {
+    // create an array that can hold up to 10 pets (does not require looping)
+    Pet[] pets = new Pet[10]; 
+    // adopt (create and name) four pets, two cats, two dogs (does not require looping)
+    pets = {}
+    // first things first, feed your pets
+
+    // next, make yourself the owner of all of your new pets
+
+    // your dogs make some noise, take them for a walk
+
+    // when you get back, your cats make some noise
+    
+    // give all of your pets a treat
+
+    // groom your cats
+ 
+    // grooming is done, play with all pets
+
+    // whew, that was tiring, all pets nap and get fed
+
+    System.out.println("--- MY PETS ---");
+    // show the state of all of your pets
+    
+    // You decide to get a couple of pets for your friend (does not require looping)
+    
+    // set the owner of the new pets to your friends name
+    
+    System.out.println("--- MY PETS ---");
+    // show the state of all of your pets
+
+  }
+}
